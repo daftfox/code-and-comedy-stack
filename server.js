07352bfa310dcf1e1557895d1f5d61112621433a
Claassens <Tim.Claassens@ordina.nl>
2016-10-13 14:10:51 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+var backend = require('./back-end/app.js');
+var frontend = require('./front-end/server.js');
+
+app.use(backend);
+app.use(frontend);
+
+backend.set('port', 3000);
+frontend.set('port', process.env.PORT || 3001);
+
+// fire up the back-end server
+var backendServer = backend.listen(backend.get('port'), function() {
+    console.log('Back-end server listening on port ' + backend.get('port') + ' in ' + backend.get('env') + ' mode');
+});
+
+// Fire up the front-end server
+var frontendServer = frontend.listen(frontend.get('port'), function() {
+    console.log('Front-end server listening on port ' + frontend.get('port') + ' in ' + frontend.get('env') + ' mode');
+});
+
