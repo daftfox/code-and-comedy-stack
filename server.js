c5912c8d7b87c235263f55a1e9d42c68714fd604
Claassens <Tim.Claassens@ordina.nl>
2016-10-19 14:11:54 +0200

    reverted changes

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ backend.set('port', 3000);
 frontend.set('port', process.env.PORT || 3001);
 
 // fire up the back-end server
-var backendServer = backend.listen(backend.get('port'), '0.0.0.0', function() {
+var backendServer = backend.listen(backend.get('port'), function() {
     console.log('Back-end server listening on port ' + backend.get('port') + ' in ' + backend.get('env') + ' mode');
 });
 
 // Fire up the front-end server
-var frontendServer = frontend.listen(frontend.get('port'), '0.0.0.0', function() {
+var frontendServer = frontend.listen(frontend.get('port'), function() {
     console.log('Front-end server listening on port ' + frontend.get('port') + ' in ' + frontend.get('env') + ' mode');
 });
 
